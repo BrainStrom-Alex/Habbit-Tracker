7cacd13f2ceccd8d81fb3f1a91f8da3d50a2b814
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-09-21 21:21:14 +0530

    get request and post requests

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+TOKEN = "abc"
+USERNAME = "alex07"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "abc",
+    "name": "Working Time",
+    "unit": "minutes",
+    "type": "int",
+    "color": "momiji",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
+

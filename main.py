e6a5a9ae15bed9b7477c5e9e015833ff88eec32b
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-09-22 21:29:24 +0530

    Completed the project

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
+from datetime import datetime
 
 pixela_endpoint = "https://pixe.la/v1/users"
 TOKEN = "abc"
-USERNAME = "alex07"
+USERNAME = "abc"
+GRAPH_ID = "abc"
 
 user_params = {
     "token": TOKEN,
@@ -18,7 +20,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "abc",
+    "id": GRAPH_ID,
     "name": "Working Time",
     "unit": "minutes",
     "type": "int",
@@ -28,6 +30,31 @@ graph_config = {
 headers = {
     "X-USER-TOKEN": TOKEN,
 }
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+today = datetime.now()
+print(today.strftime("%Y%m%d"))
+
+requests_body = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "20",
+}
+
+put_body = {
+    "quantity": "15",
+}
+
+
+post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+response = requests.post(url=post_endpoint, json=requests_body, headers=headers)
 print(response.text)
+
+put_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/20260921"
+
+# response = requests.put(url=put_endpoint, json=put_body, headers=headers)
+# print(response.text)
+
+# response = requests.delete(url=put_endpoint, headers=headers)
+# print(response.text)
 
